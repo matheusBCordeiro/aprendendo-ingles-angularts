838092472d38feabb7576b450ea15c760720ab28
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-04-01 21:40:35 -0300

    Frase class

---
+++ app/src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model'
+
+export const FRASES: Frase [] = [
+    { fraseEng: 'I like to learn', frasePtBr: 'Eu gosto de aprender' },
+    { fraseEng: 'I eatch tv', frasePtBr: 'Eu assito tv' },
+    { fraseEng: 'How are you?', frasePtBr: 'Como vai você?' },
+    { fraseEng: 'I like eat hot dog', frasePtBr: 'Eu gosto de cachorro quente' },
+]
